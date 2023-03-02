22a16e9e275ff86b0c3d3b987c0dd0ce3ef4bd43
erfanzar <erfanzare82@yahoo.com>
2023-03-02 23:19:21 +0300

    adding support to read from text file

--- engine.py
+++ engine.py
@@ -4,6 +4,7 @@ import typing
 from typing import Union, List, Optional
 
 import numpy as np
+from tqdm.auto import tqdm
 import torch
 
 from baseline import generate, MAXIMUM_RES
@@ -13,9 +14,9 @@ pars = argparse.ArgumentParser()
 
 pars.add_argument('--model-path', '--model-path', default=r'E:\CGRModel-checkpoints', type=str)
 pars.add_argument('--prompts', '--prompts', type=str, nargs='+',
-                  default='inside house design architects,realistic,detailed,sharp,smooth')
+                  default='prompts.txt')
 pars.add_argument('--save-dir', '--save-dir', type=str, default='out')
-pars.add_argument('--step', '--step', type=int, default=1)
+pars.add_argument('--step', '--step', type=int, default=4)
 pars.add_argument('--device', '--device', type=str, default='cuda' if torch.cuda.is_available() else 'cpu')
 pars.add_argument('--size', '--size', type=int, default=512)
 opt = pars.parse_args()
@@ -24,32 +25,43 @@ opt = pars.parse_args()
 def config_model(model_path: Union[str, os.PathLike],
                  device: Union[torch.device, str] = 'cuda' if torch.cuda.is_available() else 'cpu',
                  nsfw_allowed: Optional[bool] = True):
-    data_type = torch.float32 if device != 'cuda' else torch.float16
-    model = CGRModel.from_pretrained(model_path, torch_dtype=data_type).to(device)
-    if nsfw_allowed:
-        model.safety_checker.to('cpu')
+    # data_type = torch.float32 if device != 'cuda' else torch.float16
+    # model = CGRModel.from_pretrained(model_path, torch_dtype=data_type).to(device)
+    # if nsfw_allowed:
+    #     model.safety_checker.to('cpu')
+    #
+    # return model
+    return None
 
-    return model
 
-
-def main(model: Optional[CGRModel], prompts: Union[str, List[str]], size: Optional[typing.Tuple],
+def main(model: Optional[CGRModel], prompts: Union[str, List[str], os.PathLike], size: Optional[typing.Tuple],
          using_step: Optional[bool] = True, nsfw_allowed: Optional[bool] = True, out_dir: Optional[str] = 'out',
          step: Optional[int] = 2):
     kwargs = dict(use_version=True, version='v4', use_realistic=False, size=size, nsfw_allowed=nsfw_allowed,
                   out_dir=out_dir)
 
-    if isinstance(prompts, str):
+    if isinstance(prompts, str) and not prompts.endswith('.txt'):
+
         generate(prompt=prompts, model=model, **kwargs)
-    elif isinstance(prompts, list):
+
+    elif isinstance(prompts, list) or prompts.endswith('.txt'):
+
+        if prompts.endswith('.txt'):
+            us_a: bool = True
+            prompts = open(prompts, 'r', encoding='utf8').readlines()
+        else:
+            us_a: bool = False
         if using_step:
             prompts = np.array(prompts)
             bl = len(prompts) % step if len(prompts) > step else step - len(prompts)
+            if us_a:
+                bl += 2
             if len(prompts) % step != 0:
                 v = np.array(['<BREAK>' for _ in range(bl)])
                 prompts = np.concatenate((prompts, v), axis=0)
             prompts = prompts.reshape((len(prompts) // step if len(prompts) // step != 0 else 1, step))
-            for i, prp in enumerate(prompts):
-
+            for i, prp in tqdm(enumerate(prompts)):
+                # print(prp)
                 if '<BREAK>' in prp:
                     fx = 0
                     for ix, p in enumerate(prp):
